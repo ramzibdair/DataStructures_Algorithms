77fac7aec0bb9955c37da6697df3e9525d30aeb2
ramzi <ramzi@LAPTOP-MVODADDD>
2021-12-13 00:03:18 +0300

    add Depth First Search Algorith

---
+++ DataStructures_Algorithms/Graph/DFS/Graph.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFS
+{
+    internal class Graph
+    {
+        private readonly int Vertices;
+        private List<int>[] Edges;
+
+        public Graph(int verties)
+        {
+            Vertices = verties;
+            Edges = new List<int>[verties];
+        }
+        public void AddEdge(int source ,int[] destination)
+        {
+            Edges[source] = new List<int>();
+            Edges[source].AddRange(destination);
+        }
+        public void DFSUtility(int vertix,bool[] visited )
+        {
+            if (!visited[vertix])
+            {
+                Console.Write($"{vertix} ");
+                visited[vertix] = true;
+                foreach (var edge in Edges[vertix])
+                {
+                    DFSUtility(edge, visited);
+                }
+            }
+            
+        }
+
+        public void DFS(int root)
+        {
+            bool[] visited = new bool[Vertices];
+            Console.Write($"{root} ");
+            visited[root] = true;
+            foreach(var edge in Edges[root])
+            {
+                DFSUtility(edge, visited);
+            }
+        }
+    }
+}
---
+++ DataStructures_Algorithms/Graph/DFS/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DFS;
+
+Console.WriteLine(" \x1b[1mInput\x1b[0m : n = 4, e = 6 ");
+Console.WriteLine("\x1b[1mEdges\x1b[0m : 0 -> 1, 0 -> 2, 1 -> 2, 2 -> 0, 2 -> 3, 3 -> 3 ");
+Console.WriteLine("\x1b[1mOutput\x1b[0m: DFS from vertex 1 : 1 2 0 3 \n\n");
+Graph g = new Graph(4);
+
+g.AddEdge(0,new int[2] { 1,2 });
+g.AddEdge(1, new int[1] { 2 });
+g.AddEdge(2, new int[2] { 0,3 });
+g.AddEdge(3, new int[1] {  3 });
+
+Console.WriteLine( "Following is the result of our algorithm:");
+
+g.DFS(1);
+Console.ReadKey();
